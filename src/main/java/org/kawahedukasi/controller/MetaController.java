9aace6e010d4128caf4cedc902c5c0a689f48797
Ditaanggraeni <ditaa1099@gmail.com>
2023-03-28 23:32:16 +0700

    Get list category, bank and detailBank

--- src/main/java/org/kawahedukasi/controller/MetaController.java
+++ src/main/java/org/kawahedukasi/controller/MetaController.java
@@ -1,22 +1,24 @@
 package org.kawahedukasi.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.kawahedukasi.constant.Gender;
+import org.kawahedukasi.constant.ItemCategory;
 import org.kawahedukasi.model.JobPosition;
 import org.kawahedukasi.model.LastEducation;
 import org.kawahedukasi.model.PaymentType;
 import org.kawahedukasi.service.MetaService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
 
 @Path("/api/meta")
 @Produces(MediaType.APPLICATION_JSON)
@@ -59,5 +61,32 @@ public class MetaController {
     })
     public Response gender(){
         return metaService.gender();
+    }
+
+    @GET
+    @Path("/itemCategory")
+    @APIResponses({
+            @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ItemCategory.class)))
+    })
+    public Response itemCategory(){
+        return metaService.itemCategory();
+    }
+
+    @GET
+    @Path("/bank")
+    @APIResponses({
+            @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = List.class)))
+    })
+    public Response bank() throws FileNotFoundException, JsonProcessingException {
+        return metaService.bank();
+    }
+
+    @GET
+    @Path("/bank/{code}")
+    @APIResponses({
+            @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = Map.class)))
+    })
+    public Response detailBank(@PathParam("code") String code) throws FileNotFoundException, JsonProcessingException {
+        return metaService.detailBank(code);
     }
 }
--- src/main/java/org/kawahedukasi/service/MetaService.java
+++ src/main/java/org/kawahedukasi/service/MetaService.java
@@ -1,16 +1,24 @@
 package org.kawahedukasi.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.kawahedukasi.constant.Gender;
+import org.kawahedukasi.constant.ItemCategory;
+import org.kawahedukasi.exception.ValidationException;
 import org.kawahedukasi.model.JobPosition;
 import org.kawahedukasi.model.LastEducation;
 import org.kawahedukasi.model.PaymentType;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class MetaService {
+    ObjectMapper om = new ObjectMapper();
     public Response jobPosition(){
         return Response.ok(JobPosition.findAll().list()).build();
     }
@@ -32,5 +40,40 @@ public class MetaService {
             result.add(map);
         }
         return Response.ok(result).build();
+    }
+
+    public Response itemCategory(){
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (ItemCategory itemCategory:ItemCategory.values()){
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", itemCategory.getName());
+            result.add(map);
+        }
+        return Response.ok(result).build();
+    }
+
+    public Response bank() throws FileNotFoundException, JsonProcessingException {
+        return Response.ok(getBankFromResources()).build();
+    }
+
+    public List<Map<String, Object>> getBankFromResources() throws FileNotFoundException, JsonProcessingException {
+        File file = new File("./src/main/resources/bank.json");
+        Scanner scanner = new Scanner(file);
+        StringBuilder sb = new StringBuilder();
+        while (scanner.hasNextLine()){
+            sb.append(scanner.nextLine());
+        }
+        scanner.close();
+        return om.readValue(sb.toString(), List.class);
+    }
+
+    public Response detailBank(String code) throws FileNotFoundException, JsonProcessingException {
+        List<Map<String, Object>> bankList = getBankFromResources()
+                .stream().filter(s -> s.get("bank_code").toString().equals(code))
+                .collect(Collectors.toList());
+        if (code.isEmpty()){
+            throw new ValidationException("INVALID_CODE");
+        }
+        return Response.ok(bankList.get(0)).build();
     }
 }
